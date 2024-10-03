f1dc40a1286bfac7e188f0c5668f523a7732ad69
Semas2001 <semasarmonaitis@gmail.com>
2024-10-03 14:06:39 +0100

    changes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const nextConfig = {
-  output: 'export',
-  reactStrictMode: true,
   webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.devtool = process.env.NODE_ENV === 'development' ? 'source-map' : false;
-    }
+    config.devtool = 'source-map'; 
     return config;
   },
 };
-
-
 
 module.exports = withSentryConfig(nextConfig, {
   org: "semas-armonaitis",
