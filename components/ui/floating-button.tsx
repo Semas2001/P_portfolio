80c1799d850b976ad53552d27aec905d5bc97f27
Semas2001 <semasarmonaitis@gmail.com>
2024-10-04 13:02:16 +0100

    moved the download CV button to the front

--- components/ui/floating-button.tsx
+++ components/ui/floating-button.tsx
@@ -15,7 +15,7 @@ const FloatingButton = () => {
   return (
     <button
       onClick={handleDownload}
-      className="fixed z-45 bottom-4 left-5 inline-flex h-14 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+      className="fixed z-30 bottom-4 left-5 inline-flex h-14 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
       aria-label="Download CV"
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
